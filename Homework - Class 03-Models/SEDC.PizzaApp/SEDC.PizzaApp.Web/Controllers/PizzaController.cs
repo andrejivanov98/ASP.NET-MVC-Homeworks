6749b2427a397bdd5bb651bd7840aca3f350b329
Andrej Ivanov <ivanovandrej1995@gmail.com>
2021-08-12 00:42:55 +0200

    Models homework

---
+++ Homework - Class 03-Models/SEDC.PizzaApp/SEDC.PizzaApp.Web/Controllers/PizzaController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SEDC.PizzaApp.Web.Models.Domain;
+using SEDC.PizzaApp.Web.Models.Enums;
+using SEDC.PizzaApp.Web.Models.Mapper;
+using SEDC.PizzaApp.Web.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SEDC.PizzaApp.Web.Controllers
+{
+    public class PizzaController : Controller
+    {
+        public IActionResult Index(int id)
+        {
+
+            List<Pizza> pizzas = StaticDB.Pizzas;
+            List<PizzaViewModel> pizzaDetails = new List<PizzaViewModel>();
+
+            foreach (Pizza pizza in pizzas)
+            {
+                pizzaDetails.Add(PizzaMapper.PizzaToPizzaViewModel(pizza));
+            }
+
+            Pizza newPizza = new Pizza()
+            {
+                Id = 4,
+                Name = "Vegetarijana",
+                Price = 6,
+                IsOnPromotion = true,
+                HasExtras = true,
+                PizzaSize = PizzaSize.Normal
+            };
+
+            pizzaDetails.Add(PizzaMapper.PizzaToPizzaViewModel(newPizza));
+
+            ViewData.Add("Title", "List of Pizzas");
+            return View(pizzaDetails);
+        }
+
+        public IActionResult Details(int id)
+        {
+            List<Pizza> pizzas = StaticDB.Pizzas;
+            List<PizzaViewModel> pizzaDetails = new List<PizzaViewModel>();
+            foreach (Pizza pizza in pizzas)
+            {
+                pizzaDetails.Add(PizzaMapper.PizzaToPizzaViewModel(pizza));
+            }
+
+            foreach (PizzaViewModel pizza in pizzaDetails)
+            {
+                if (pizza.Id == id)
+                {
+
+                    ViewData.Add("Title", "Pizza name");
+                    ViewBag.PizzaViewModel = pizza;
+                    return View();
+                }
+
+
+            }
+            return new EmptyResult();
+
+        }
+
+
+
+    }
+}
---
+++ Homework - Class 03-Models/SEDC.PizzaApp/SEDC.PizzaApp.Web/Models/Mapper/PizzaMapper.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SEDC.PizzaApp.Web.Models.Domain;
+using SEDC.PizzaApp.Web.Models.Enums;
+using SEDC.PizzaApp.Web.Models.Mapper;
+using SEDC.PizzaApp.Web.Models.ViewModels;
+using System.Collections.Generic;
+using System.Linq;
+
+
+
+namespace SEDC.PizzaApp.Web.Models.Mapper
+{
+    public class PizzaMapper
+    {
+        public static PizzaViewModel PizzaToPizzaViewModel(Pizza pizza)
+        {
+            return new PizzaViewModel
+            {
+                Id = pizza.Id,
+                Name = pizza.Name,
+                Price = pizza.Price,
+                PizzaSize = pizza.PizzaSize,
+                HasExtras = pizza.HasExtras
+
+
+
+            };
+        }
+    }
+}
