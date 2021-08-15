e9d4389408a7be0390da44f03de23a2f99ddb877
Andrej Ivanov <ivanovandrej1995@gmail.com>
2021-08-16 00:50:28 +0200

    Homework-Viwes part 01 added

---
+++ Homework - Class 04 - Views part 01/SEDC.PizzaApp/SEDC.PizzaApp.Web/Controllers/PizzaController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SEDC.PizzaApp.Web.Models.Domain;
+using SEDC.PizzaApp.Web.Models.Enums;
+using SEDC.PizzaApp.Web.Models.Mapper;
+using SEDC.PizzaApp.Web.Models.ViewModels;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SEDC.PizzaApp.Web.Controllers
+{
+    public class PizzaController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Pizza> pizzas = StaticDB.Pizzas;
+            List<PizzaViewModel> listOfPizzas = new List<PizzaViewModel>();
+           
+            foreach(Pizza pizza in pizzas)
+            {
+                listOfPizzas.Add(PizzaMapper.PizzaToPizzaViewModel(pizza));
+            }
+            return View(listOfPizzas);
+
+        
+        }
+
+        public IActionResult PizzaDetailsView()
+        {
+            PizzaViewModel newPizza = new PizzaViewModel()
+            {
+                Id = 1,
+                Name = "Calcone",
+                IsOnPromotion = true,
+                Price = 10
+            };
+
+            return View(newPizza);
+        }
+    }
+}
---
+++ Homework - Class 04 - Views part 01/SEDC.PizzaApp/SEDC.PizzaApp.Web/Models/Mapper/PizzaMapper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SEDC.PizzaApp.Web.Models.Domain;
+using SEDC.PizzaApp.Web.Models.ViewModels;
+
+
+namespace SEDC.PizzaApp.Web.Models.Mapper
+{
+    public class PizzaMapper
+    {
+        public static PizzaViewModel PizzaToPizzaViewModel(Pizza pizza)
+        {
+            return new PizzaViewModel
+            {
+                Id = pizza.Id,
+                Name = pizza.Name,
+                IsOnPromotion = pizza.IsOnPromotion,
+                Price = pizza.Price
+
+            };
+
+
+        }
+    }
+}
